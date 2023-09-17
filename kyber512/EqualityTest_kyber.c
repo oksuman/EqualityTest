2db2e5360818918be325f4854fb70070353e331b
oksuman <johndoe@example>
2023-09-18 07:09:34 +0900

    kyber added

---
+++ kyber512/EqualityTest_kyber.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include "indcpa.h"
+#include "params.h"
+
+const unsigned char* generateCoin() {
+    static unsigned char seed[KYBER_SYMBYTES]; 
+
+    for (size_t i = 0; i < KYBER_SYMBYTES; ++i) {
+        seed[i] = rand() % 256; 
+    }
+    return seed;
+}
+
+
+int main(int argc, char* argv[]){
+    printf("its okay 0\n");
+    const unsigned char* coin = generateCoin();
+
+    unsigned char msg1[KYBER_INDCPA_MSGBYTES] = {0}; 
+    unsigned char msg2[KYBER_INDCPA_MSGBYTES] = {0};
+    unsigned char msg3[KYBER_INDCPA_MSGBYTES] = {0};
+
+    msg1[KYBER_INDCPA_MSGBYTES-1] = 197;
+    msg2[KYBER_INDCPA_MSGBYTES-1] = 197;
+
+    
+    unsigned char ctx1[KYBER_INDCPA_BYTES];
+    unsigned char ctx2[KYBER_INDCPA_BYTES];
+    unsigned char ctx3[KYBER_INDCPA_BYTES];
+
+    unsigned char pk[KYBER_INDCPA_PUBLICKEYBYTES];
+    unsigned char sk[KYBER_INDCPA_SECRETKEYBYTES];
+    printf("its okay 1\n");
+    indcpa_keypair(pk, sk);
+
+    printf("its okay 2\n");
+    indcpa_enc(ctx1, msg1, pk, coin);
+    indcpa_enc(ctx2, msg2, pk, coin);
+
+
+    printf("its okay 3\n");
+    add(ctx3, ctx1, ctx2);
+    
+    indcpa_dec(msg3, ctx3, sk);
+    
+    
+    printf("msg1 contents: ");
+    for (int i = 0; i < KYBER_INDCPA_MSGBYTES; i++) {
+        printf("%02X ", msg1[i]);
+    }
+    printf("\n");
+    
+    
+    printf("msg2 contents: ");
+    for (int i = 0; i < KYBER_INDCPA_MSGBYTES; i++) {
+        printf("%02X ", msg2[i]);
+    }
+    printf("\n");
+    
+    printf("msg3 contents: ");
+    for (int i = 0; i < KYBER_INDCPA_MSGBYTES; i++) {
+        printf("%02X ", msg3[i]);
+    }
+    printf("\n");
+  
+    
+    
+    return 0;
+}
+
+// gcc -o EqualityTest_kyber EqualityTest_kyber.c indcpa.c -lssl -lcrypto -lrt
+
+
+
---
+++ kyber512/indcpa.h
@@ -0,0 +1,19 @@
+#ifndef INDCPA_H
+#define INDCPA_H
+
+void indcpa_keypair(unsigned char *pk,
+                    unsigned char *sk);
+
+void indcpa_enc(unsigned char *c,
+                const unsigned char *m,
+                const unsigned char *pk,
+                const unsigned char *coins);
+
+void indcpa_dec(unsigned char *m,
+                const unsigned char *c,
+                const unsigned char *sk);
+
+void add(unsigned char *c, unsigned char*c1, unsigned char* c2);
+
+
+#endif
